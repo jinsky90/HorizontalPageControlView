0e38c8cd6647290b826045fe2942962eaa57c196
sky <hnjin@station3.co.kr>
2019-07-31 15:28:29 +0900

    set carthage

---
+++ HorizontalPageControlView/HorizontalPageControlView.h
@@ -0,0 +1,19 @@
+//
+//  HorizontalPageControlView.h
+//  HorizontalPageControlView
+//
+//  Created by 진하늘 on 31/07/2019.
+//  Copyright © 2019 진하늘. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HorizontalPageControlView.
+FOUNDATION_EXPORT double HorizontalPageControlViewVersionNumber;
+
+//! Project version string for HorizontalPageControlView.
+FOUNDATION_EXPORT const unsigned char HorizontalPageControlViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HorizontalPageControlView/PublicHeader.h>
+
+
